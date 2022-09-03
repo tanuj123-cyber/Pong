3deefe68a3043de574dd3e1664236af1cb801a74
Tanuj Sistla <tvs9709@nyu.edu>
2022-09-03 15:57:19 -0400

    Window Working

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import pygame
-
+import window
 
 def main():
 
     pygame.init()
     surface = pygame.display.set_mode((400,300))
-    pygame.display.set_caption('Hello world')
 
 
     while True:
@@ -18,4 +17,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    window.window()
+    
--- src/window.py
+++ src/window.py
@@ -1,18 +1,20 @@
 
 import pygame
 
-background = (0,0,0)
+def window():
+    background = (0,0,0)
 
-screen = pygame.display.set_model((300,300))
+    screen = pygame.display.set_mode((300,300))
 
-pygame.display.set_caption("Pong")
+    pygame.display.set_caption("Pong")
 
-screen.fill(background)
+    screen.fill(background)
 
-pygame.display.flip()
-running = True
+    pygame.display.flip()
+    running = True
 
-while(running):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    while(running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    return
